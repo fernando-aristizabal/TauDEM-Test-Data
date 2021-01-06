b011c4176616498a3ea84090cad8c2c9f768d15d
fernando-aristizabal <fernandoa@disroot.org>
2021-01-06 12:23:17 +0000

    Tolerance change

--- compare_rasters.py
+++ compare_rasters.py
@@ -10,7 +10,7 @@ tst_file=sys.argv[2]
 ref_obj=rasterio.open(ref_file)
 tst_obj=rasterio.open(tst_file)
 
-assert isclose(ref_obj.read(1),tst_obj.read(1),atol=1e-3).all(), "{} and {} raster values are NOT identical".format(ref_file,tst_file)
+assert isclose(ref_obj.read(1),tst_obj.read(1),atol=1e-4).all(), "{} and {} raster values are NOT identical".format(ref_file,tst_file)
 
 print("{} and {} raster values ARE identiifcal".format(ref_file,tst_file))
 
